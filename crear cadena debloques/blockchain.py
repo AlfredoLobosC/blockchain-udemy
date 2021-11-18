be44561bf4946834084e13ba4fc367ab71b3f375
AlfredoLobosC <alfredo.lobos@gmail.com>
2021-11-17 21:00:56 -0300

    First file in module 1

--- crear cadena debloques/blockchain.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 18 20:46:44 2021
-
-@author: alfre
-"""
-
-# Modulo 1 - Crear una cadena de bloques
---
+++ crearCadenaDeBloques/blockchain.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 20:56:02 2021
+
+@author: alfre
+"""
+
+# Module 1 - Crear una cadena de bloques
